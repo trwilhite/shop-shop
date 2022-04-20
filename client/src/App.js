8fde9a745cd0afbc8a5e002d292920a2f47a5cd6
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-04-20 18:49:53 -0400

    added stripe checkout

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Success from "./pages/Success";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
@@ -49,6 +50,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
